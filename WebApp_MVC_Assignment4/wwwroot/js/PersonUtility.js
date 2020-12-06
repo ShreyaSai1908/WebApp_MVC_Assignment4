151880e300ed88e4c6dd601af24c195c758b4a01
Editting <deltagare@LEXJONKAF33>
2020-12-06 02:45:55 +0100

    Comments & loggig removed

--- WebApp_MVC_Assignment4/wwwroot/js/PersonUtility.js
+++ WebApp_MVC_Assignment4/wwwroot/js/PersonUtility.js
@@ -34,19 +34,9 @@ function PostAddPersonForm(event, form)
 }
 
 function GetEditPersonForm(urlToEditForm, personID) {
-    console.log("urlToEditForm:", urlToEditForm);
-    console.log("personID:", personID);
-
-    //personListArea = $("#peopleListArea").children();
-    //editedPersonArea = personListArea[personID];
-
     editedPersonArea = $("#personRowID-" + personID);
-    
-
     $.get(urlToEditForm + "/" + personID, function (result) {
         editedPersonArea.html(result);
-        //editedPersonArea.innerHTML = result;
-        //console.log("Edit Person Form", editedPersonArea.innerHTML);
     })
 }
 
@@ -62,38 +52,16 @@ function PostEditPersonForm(event, form) {
             Address: form.Address.value
         },
         function (data, status) {
-            console.log(data);
             $(editedPersonArea).replaceWith(data);
-
-            /*
-            //element = editedPersonArea;
-            //element.innerHTML = data;
-
-            //console.log("Edit Person View", editedPersonArea.innerHTML);
-            //editedPersonArea.innerHTML = data;
-            */
-            
-            // $("#toggleBtnAddPersonForm").html(btnAddPerson);
 
         }).fail(function (badForm) {
             console.log("badForm: Fail");
-            //$("#createCarDiv").html(badForm.responseText);
         });
 }
 
 function RemovePerson(urlToRemovePerson, personID) {
-    console.log("urlToRemovePerson:", urlToRemovePerson);
-    console.log("personID:", personID);
-
-    //personListArea = $("#peopleListArea").children();
-    //editedPersonArea = personListArea[personID];
-
     removedPersonArea = $("#personRowID-" + personID);
-    //console.log("Remove Person", removedPersonArea.tagName);
-
     $.get(urlToRemovePerson + "/" + personID, function (result) {
-        //editedPersonArea.replaceWith(result);
-        //editedPersonArea.innerHTML = result;
         removedPersonArea.replaceWith(result);
     })
 }
@@ -111,6 +79,5 @@ function SearchPeople(event, form) {
             
         }).fail(function (badForm) {
             console.log("badForm: Fail");
-            //$("#createCarDiv").html(badForm.responseText);
         });
 }
